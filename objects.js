2d5d22cdc1424b6b5102e676da8e51461d93c3a0
Justin Daniel <justinddaniel@gmail.com>
2017-08-24 00:23:46 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,7 @@
 var playlist = { Prince: "Messiah", MJackson: "Bad", Madonna: "Like a prayer"};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+  
